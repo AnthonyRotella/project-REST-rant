f0e7ab41902ba0107fd5098d9e0c9f7f5a7ec0d2
Anthony Rotella <mr4406@yahoo.com>
2022-05-18 23:10:51 -0500

    Form data is getting through

--- Controllers/places.js
+++ Controllers/places.js
@@ -13,7 +13,10 @@ let places = [{
     cuisines: 'Sweets',
     pic: '/images/theyum.jpg'
 }]
-
+router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
+})
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
